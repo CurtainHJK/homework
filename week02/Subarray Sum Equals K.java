eb2c47caa3c30c7879522778c2d53fcf4b5aaca1
CurtainHJK <1509458871@qq.com>
2021-10-17 22:50:01 +0800

    第二周作业

---
+++ week02/Subarray Sum Equals K.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int len = nums.length;
+        int[] preSum = new int[len + 1];
+        preSum[0] = 0;
+        //前缀和
+        for (int i = 0; i < len; i++) {
+            preSum[i + 1] = preSum[i] + nums[i];
+        }
+
+        int count = 0;
+        //计算符合的个数
+        for (int left = 0; left < len; left++) {
+            for (int right = left; right < len; right++) {
+                if (preSum[right + 1] - preSum[left] == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
